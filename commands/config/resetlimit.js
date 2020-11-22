4df68b7e2b88d5b897324f7726fcde583325b5d4
dftd <lil.funk.133@gmail.com>
2020-11-22 14:49:29 +0100

    reset limit

---
+++ commands/config/resetlimit.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'resetlimit',
+    description: 'Reset application limit of an user.',
+    usage: 'resetlimit <user>',
+    aliases: [],
+    permissions: ['ADMINISTRATOR'],
+    botPermissions: [],
+    nsfw: false,
+    cooldown: 0,
+    ownerOnly: false
+}
+
+module.exports.execute = async(bot, msg, args, data) => {
+    let search = args.join(' ').trim().toLowerCase();
+    let user = bot.tools.getUserMention(msg, search);
+
+    if(!user)
+        return bot.embeds.cmdError(msg, 'Specify a valid user by mentioning or writing the username.', module.exports);
+
+
+    let memberDB = await bot.data.getMemberDB(user.id, msg.guild.id);
+    memberDB.applyCount = 0;
+    memberDB.rejectCount = 0;
+    await memberDB.save();
+
+    let embed = new Discord.MessageEmbed()
+        .setColor(bot.config.color)
+        .setDescription('Application limit for ' + user.tag + ' has been reset.')
+    return msg.channel.send(embed);
+}
--- helpers/tools.js
+++ helpers/tools.js
@@ -10,3 +10,25 @@ module.exports.getLogChannel = function(guild, data) {
 
     return channel;
 };
+
+module.exports.getUserMention = function(msg, search) {
+    let user;
+
+    if(msg.mentions.members.first()) { // @Defected
+        user = msg.mentions.members.first();
+    } else if(search.includes('#')) { // Defected#0001
+        let split = search.split('#');
+        let lookup = msg.guild.members.cache.find(u => u.user.username.toLowerCase() == split[0] && u.user.discriminator == split[1]);
+        if(lookup) user = lookup;
+    } else if(search != '') {
+        let lookup = msg.guild.members.cache.find(u => u.user.username.toLowerCase() == search); // Defected
+        if(lookup) {
+            user = lookup;
+        } else {
+            let lookup = msg.guild.members.cache.find(u => u.user.username.toLowerCase().startsWith(search)); // Def
+            if(lookup) user = lookup;
+        }
+    }
+
+    return (user) ? user.user : false;
+}
